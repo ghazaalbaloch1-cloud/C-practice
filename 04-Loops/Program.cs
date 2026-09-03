a7b6adb2007c69a4137dcf0209ddec6c6df4ff04
ghazaalbaloch1-cloud <ghazaalbaloch1@gmail.com>
2026-09-03 09:11:33 +0500

    Add Loops exercises

---
+++ 04-Loops/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Numbers 1 to 10:");
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+
+        // TODO: Print even numbers from 1 to 100.
+        // TODO: Print a multiplication table for a number entered by the user.
+        // TODO: Use a while loop to calculate the sum from 1 to N.
+    }
+}
