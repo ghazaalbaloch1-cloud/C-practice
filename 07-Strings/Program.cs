137d4c4f003de51933fcaf0051f47263765a9732
ghazaalbaloch1-cloud <ghazaalbaloch1@gmail.com>
2026-09-03 09:11:57 +0500

    Add Strings exercises

---
+++ 07-Strings/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string text = "CSharp Practice";
+        Console.WriteLine($"Length: {text.Length}");
+        Console.WriteLine($"Upper: {text.ToUpper()}");
+
+        // TODO: Count vowels in a string.
+        // TODO: Reverse a string.
+        // TODO: Check whether a word is a palindrome.
+    }
+}
