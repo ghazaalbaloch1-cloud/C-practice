4fe70cd674ff0de324827d2c746a56582c4469fe
ghazaalbaloch1-cloud <ghazaalbaloch1@gmail.com>
2026-09-03 09:11:40 +0500

    Add Arrays exercises

---
+++ 05-Arrays/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+
+        Console.WriteLine("Array values:");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+
+        // TODO: Calculate the sum and average of the array.
+        // TODO: Find the largest and smallest value.
+        // TODO: Search for a number entered by the user.
+    }
+}
